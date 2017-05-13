10097cc257ccf9ea44ce3bd90e039159a5dae85a
Phil Sautter <psautter@gmail.com>
2017-05-13 10:52:18 -0400

    server and html routes

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    PORT = process.argv[2];
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+require('./app/routing')(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
